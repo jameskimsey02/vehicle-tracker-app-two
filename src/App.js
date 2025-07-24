23165ca779a40b924213973fa9b111112cf425c2
James Kimsey <james@oregonmobility.com>
2025-07-24 15:52:03 -0700

    Initial push from packaged app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import VehicleTracker from "./VehicleTracker";
+
+function App() {
+  return (
+    <div>
+      <VehicleTracker />
+    </div>
+  );
+}
+
+export default App;
